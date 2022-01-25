92354e66ad268c92949b3aa836db0cd4bf7c73ce
Berenika Ahmetaj <ahmetajberenika@gmail.com>
2022-01-25 17:03:22 -0500

    Created the required components and setup the routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import React from 'react';
-import './App.css';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import LogIn from "./components/LogIn";
+import FriendsList from "./components/FriendsList";
+import AddFriend from "./components/AddFriend";
 
 function App() {
   return (
-    <div className="App">
-      <h2>Client Auth Project</h2>
-    </div>
+    <Router>
+      <div className="App">
+        <Route exact path="/">
+          <LogIn />
+        </Route>
+        <Route exact path="/login">
+          <LogIn />
+        </Route>
+        <Route exact path="/friends">
+          <FriendsList />
+        </Route>
+        <Route exact path="/friends/add">
+          <AddFriend />
+        </Route>
+      </div>
+    </Router>
   );
 }
 
