00ee3ebf7abb76649625fb354004fb29cc4d1d12
Berenika Ahmetaj <ahmetajberenika@gmail.com>
2022-01-25 20:36:12 -0500

    made a call to the approparate api endpoint with new friend data and redirects you the friends list route with the new data in it

--- src/components/AddFriend.js
+++ src/components/AddFriend.js
@@ -1,18 +1,54 @@
-import React from "react";
-
+import React, { useState } from "react";
+import axios from "axios";
+import { useHistory } from "react-router-dom";
 const AddFriend = () => {
+  const { push } = useHistory();
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+  });
+
+  const handleChange = (e) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const token = localStorage.getItem("token");
+    axios
+      .post("http://localhost:9000/api/friends", form, {
+        headers: {
+          authorization: token,
+        },
+      })
+      .then((res) => {
+        push("/friends");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
   return (
     <div>
       <h2>AddFriend</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>Friend Name: </label>
-        <input style={{ height: "20px" }} type="text" name="name" />
+        <input
+          style={{ height: "20px" }}
+          onChange={handleChange}
+          type="text"
+          name="name"
+        />
         <br />
         <label>Friend Email: </label>
         <input
           style={{ marginTop: "7px", height: "20px" }}
           type="email"
           name="email"
+          onChange={handleChange}
         />
         <br />
         <button
